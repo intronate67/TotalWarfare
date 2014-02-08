9c6a4e4fe3b642ad1187de46b112d6a419e5e334
Hunter Sharpe <huntersharpe@me.com>
2014-02-08 03:52:25 -0600

    Update TotalWarfare.java

--- src/me/intronate67/TotalWarfare/TotalWarfare.java
+++ src/me/intronate67/TotalWarfare/TotalWarfare.java
@@ -1,5 +1,14 @@
 package me.intronate67.TotalWarfare;
 
+import me.intronate67.TotalWarfare.listeners.ArmorRemove;
+import me.intronate67.TotalWarfare.listeners.BlockBreak;
+import me.intronate67.TotalWarfare.listeners.LobbySign;
+import me.intronate67.TotalWarfare.listeners.PlayerAttack;
+import me.intronate67.TotalWarfare.listeners.PlayerDeath;
+import me.intronate67.TotalWarfare.listeners.PlayerLeave;
+import me.intronate67.TotalWarfare.listeners.PlayerLoseHunger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TotalWarfare extends JavaPlugin{
@@ -10,6 +19,15 @@ public class TotalWarfare extends JavaPlugin{
 		CommandManager cm = new CommandManager();
 		cm.setup();
 		getCommand("tw").setExecutor(cm);
+		
+		Bukkit.getServer().getPluginManager().registerEvents(new ArmorRemove(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new BlockBreak(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new LobbySign(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerAttack(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerLeave(), this);
+		Bukkit.getServer().getPluginManager().registerEvents(new PlayerLoseHunger(), this);
+		
 	}
 
 }
