a5b047a01f00a497eeb10e332d9ec25c5c51ffd4
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 18:59:16 -0600

    Update ForceStart.java

--- src/me/intronate67/TotalWarfare/cmds/ForceStart.java
+++ src/me/intronate67/TotalWarfare/cmds/ForceStart.java
@@ -1,51 +1,56 @@
 package me.intronate67.TotalWarfare.cmds;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import me.intronate67.TotalWarfare.Arena;
+import me.intronate67.TotalWarfare.Arena.ArenaState;
 import me.intronate67.TotalWarfare.ArenaManager;
 import me.intronate67.TotalWarfare.MessageManager;
+import me.intronate67.TotalWarfare.SettingsManager;
 
 public class ForceStart extends SubCommand{
 
-	public void onCommand(Player p, String[] args){
-		if(args.length == 0){
+	public void onCommand(Player p, String[] args) {
+		if (args.length == 0) {
 			MessageManager.getInstance().severe(p, "You must specify an arena ID.");
 			return;
 		}
+		
 		int id = -1;
-		try{ id = Integer.parseInt(args[0]); }
+		
+		try { id = Integer.parseInt(args[0]); }
 		catch (Exception e) {
-			MessageManager.getInstance().severe(p, args[0] + " is not a valid arena!");
+			MessageManager.getInstance().severe(p, args[0] + " is not a valid number!");
 			return;
 		}
 		
 		Arena a = ArenaManager.getInstance().getArena(id);
 		
-		if(a == null){
+		if (SettingsManager.getInstance().<ConfigurationSection>get(id + "") == null) {
 			MessageManager.getInstance().severe(p, "There is no arena with ID " + id + "!");
 			return;
 		}
 		
-		if(a.isStarted()){
-			MessageManager.getInstance().severe(p, "Arena " + id + " has not yet started!");
+		if (a.getState() == ArenaState.STARTED) {
+			MessageManager.getInstance().severe(p, "Arena " + id + " has already started!");
 			return;
 		}
 		
-		a.stop(null);
-		MessageManager.getInstance().good(p, "Force stopped Arena " + a.getID() + "!");
+		a.start();
+		MessageManager.getInstance().good(p, "Force started Arena " + a.getID() + "!");
 	}
 	
-	public String name(){
-		return "forcestop";
+	public String name() {
+		return "forcestart";
 	}
 	
-	public String info(){
-		return "Force stop an arena.";
+	public String info() {
+		return "Force start an arena.";
 	}
 	
-	public String[] aliases(){
-		return new String[] { "fstop", "stop" };
+	public String[] aliases() {
+		return new String[] { "fstart", "start" };
 	}
 	
 }
