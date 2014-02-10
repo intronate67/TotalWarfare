99b3dad3b042026b0c9e41335e5184dcabdf2843
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 19:00:35 -0600

    Update Join.java

--- src/me/intronate67/TotalWarfare/cmds/Join.java
+++ src/me/intronate67/TotalWarfare/cmds/Join.java
@@ -3,52 +3,50 @@ package me.intronate67.TotalWarfare.cmds;
 import me.intronate67.TotalWarfare.Arena;
 import me.intronate67.TotalWarfare.ArenaManager;
 import me.intronate67.TotalWarfare.MessageManager;
+import me.intronate67.TotalWarfare.SettingsManager;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 public class Join extends SubCommand{
 	
 	public void onCommand(Player p, String[] args) {
-		if(ArenaManager.getInstance().getArena(p) != null){
+		if (ArenaManager.getInstance().getArena(p) != null) {
 			MessageManager.getInstance().severe(p, "You are already in a game!");
 			return;
 		}
-		if(args.length == 0){
+		
+		if (args.length == 0) {
 			MessageManager.getInstance().severe(p, "You must specify an arena ID.");
 			return;
 		}
+		
 		int id = -1;
-		try{ id = Integer.parseInt(args[0]); }
+		
+		try { id = Integer.parseInt(args[0]); }
 		catch (Exception e) {
-			MessageManager.getInstance().severe(p, args[0] + " is not a valid arena!");
+			MessageManager.getInstance().severe(p, args[0] + " is not a valid number!");
 			return;
 		}
 		
 		Arena a = ArenaManager.getInstance().getArena(id);
 		
-		if(a == null){
+		if (SettingsManager.getInstance().<ConfigurationSection>get(id + "") == null) {
 			MessageManager.getInstance().severe(p, "There is no arena with ID " + id + "!");
 			return;
 		}
-		
-		if(a.isStarted()){
-			MessageManager.getInstance().severe(p, "Arena " + id + " has already started!");
-			return;
-		}
-		
 		a.addPlayer(p);
 	}
-
 	
-	public String name(){
+	public String name() {
 		return "join";
 	}
 	
-	public String info(){
+	public String info() {
 		return "Join a game.";
 	}
 	
-	public String[] aliases(){
+	public String[] aliases() {
 		return new String[] { "j" };
 	}
 }
