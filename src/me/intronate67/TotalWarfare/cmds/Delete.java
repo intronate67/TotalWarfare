8123bf80f14bd317641e3bdd95111af99c486980
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 18:58:57 -0600

    Update Delete.java

--- src/me/intronate67/TotalWarfare/cmds/Delete.java
+++ src/me/intronate67/TotalWarfare/cmds/Delete.java
@@ -4,51 +4,53 @@ import me.intronate67.TotalWarfare.Arena;
 import me.intronate67.TotalWarfare.ArenaManager;
 import me.intronate67.TotalWarfare.MessageManager;
 import me.intronate67.TotalWarfare.SettingsManager;
+import me.intronate67.TotalWarfare.Arena.ArenaState;
 
 import org.bukkit.entity.Player;
 
 public class Delete extends SubCommand{
 	
-	public void onCommand(Player p, String[] args){
-		if(args.length == 0){
-			MessageManager.getInstance().severe(p, "You mus specify an arena number!");
+	public void onCommand(Player p, String[] args) {
+		if (args.length == 0) {
+			MessageManager.getInstance().severe(p, "You must specify an arena number!");
 			return;
 		}
-
+		
 		int id = -1;
-		try{ id = Integer.parseInt(args[0]); }
+		
+		try { id = Integer.parseInt(args[0]); }
 		catch (Exception e) {
-			MessageManager.getInstance().severe(p, args[0] + " is not a valid arena!");
+			MessageManager.getInstance().severe(p, args[0] + " is not a valid number!");
 			return;
 		}
+		
 		Arena a = ArenaManager.getInstance().getArena(id);
 		
-		if(a == null){
+		if (a == null) {
 			MessageManager.getInstance().severe(p, "There is no arena with ID " + id + "!");
 			return;
 		}
 		
-		if(a.isStarted()){
-			MessageManager.getInstance().severe(p, "Arena " + id + " is in-game!");
+		if (a.getState() == ArenaState.STARTED) {
+			MessageManager.getInstance().severe(p, "Arena " + id + " is ingame!");
 			return;
 		}
 		
 		SettingsManager.getInstance().set(id + "", null);
-		
-		//TODO: Stop all games and broadcast!
+
 		ArenaManager.getInstance().setup();
-		
 	}
 	
-	public String name(){
+	public String name() {
 		return "delete";
 	}
-	public String info(){
-		return "Delete an arena!";
+	
+	public String info() {
+		return "Delete an arena.";
 	}
 	
-	public String[] aliases(){
-		return new String[]{ "d" };
+	public String[] aliases() {
+		return new String[] { "d" };
 	}
 
 }
