3b2e9edbe5e1050ae729fd6ff6ff169f7245109a
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 18:56:58 -0600

    Update Arena.java

--- src/me/intronate67/TotalWarfare/Arena.java
+++ src/me/intronate67/TotalWarfare/Arena.java
@@ -6,25 +6,22 @@ import me.intronate67.TotalWarfare.ArenaManager.Team;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Color;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class Arena extends JavaPlugin{
 	
-	private boolean started = false;
+	public enum ArenaState { DISABLED, WAITING, COUNTING_DOWN, STARTED; }
+	public boolean isHeroes = false;
+	private boolean cd = false;
 	private int id;
 	protected ArrayList<PlayerData> players = new ArrayList<PlayerData>();
 	
@@ -33,6 +30,7 @@ public class Arena extends JavaPlugin{
 	private Score heroes, demons;
 	private Location redspawn, bluespawn;
 	private Location capturePoint;
+	protected ArenaState state = ArenaState.DISABLED;
 	
 	protected Arena(int id){
 		this.id = id;
@@ -41,7 +39,9 @@ public class Arena extends JavaPlugin{
 		
 		this.redspawn = getLocation(conf.getConfigurationSection("redspawn"));
 		this.bluespawn = getLocation(conf.getConfigurationSection( "bluespawn"));
+		this.capturePoint = getCaptureLocation(conf.getConfigurationSection("capturePoint"));
 		
+		state = ArenaState.WAITING;
 		sb = Bukkit.getServer().getScoreboardManager().getNewScoreboard();
 		o = sb.registerNewObjective("Team Scores", "dummy");
 		heroes = o.getScore(Bukkit.getServer().getOfflinePlayer(ChatColor.RED + "Heroes"));
@@ -56,21 +56,27 @@ public class Arena extends JavaPlugin{
 				path.getDouble("y"),
 				path.getDouble("z"));
 	}
-	public Location getCaptureLocation(){
-		return (Location) this.getConfig().getConfigurationSection(a.getID() + "." + "capturePoint").getValues(true);
-	
+	public Location getCaptureLocation(ConfigurationSection path){
+		return new Location(
+				Bukkit.getServer().getWorld("captureWorld"),
+				path.getDouble("captureX"),
+				path.getDouble("captureY"),
+				path.getDouble("captureZ"));
+	}
+	public ArenaState getState(){
+		return state;
 	}
 	
 	public int getID(){
 		return id;
 	}
 	
-	public boolean isStarted(){
-		return started;
-	}
 	
-	public void setStarted(boolean started){
-		this.started = started;
+	public boolean isHeroes(){
+		return isHeroes;
+	}
+	public void setHeroes(boolean isHeroes){
+		this.isHeroes = isHeroes;
 	}
 	
 	public Location getSpawn(Team team){
@@ -89,100 +95,59 @@ public class Arena extends JavaPlugin{
 	}
 	
 	public void addPlayer(Player p){
-		//Heroes default class paladin.
-		// TODO: Add other hero Classes.
-		//Sword
-		ItemStack ironSword = new ItemStack(Material.IRON_SWORD);
-		ItemMeta ironMeta = ironSword.getItemMeta();
-		ironMeta.setDisplayName("Holy Light");
-		ironSword.setItemMeta(ironMeta);
-		ironSword.addEnchantment(Enchantment.DAMAGE_UNDEAD, 1);
-		//Armor
-		//Helmet
-		ItemStack lHelm = new ItemStack(Material.LEATHER_HELMET, 1);
-		LeatherArmorMeta lHelmMeta = (LeatherArmorMeta)lHelm.getItemMeta();
-		lHelmMeta.setColor(Color.WHITE);
-		//Chestplate
-		ItemStack lChest = new ItemStack(Material.LEATHER_CHESTPLATE);
-		LeatherArmorMeta lChestMeta = (LeatherArmorMeta)lChest.getItemMeta();
-		lChestMeta.setColor(Color.WHITE);
-		//Leggings
-		ItemStack lLegs = new ItemStack(Material.LEATHER_LEGGINGS);
-		LeatherArmorMeta lLegsMeta = (LeatherArmorMeta)lLegs.getItemMeta();
-		lLegsMeta.setColor(Color.WHITE);
-		//Boots
-		ItemStack lBoots = new ItemStack(Material.LEATHER_BOOTS);
-		LeatherArmorMeta lBootsMeta = (LeatherArmorMeta)lBoots.getItemMeta();
-		lBootsMeta.setColor(Color.WHITE);
-		//Demon default class Brute.
-		//Sword
-		ItemStack stoneSword = new ItemStack(Material.STONE_SWORD);
-		ItemMeta stoneMeta = stoneSword.getItemMeta();
-		stoneMeta.setDisplayName("Gnarly Claw");
-		stoneSword.setItemMeta(stoneMeta);
-		stoneSword.addEnchantment(Enchantment.DURABILITY, 10);
-		//Armor
-		ItemStack iChest = new ItemStack(Material.IRON_CHESTPLATE);
+		players.add(new PlayerData(p.getName(), getTeamWithLessPlayers(), p.getInventory().getContents(), p.getLocation()));
 		
-		players.add(new PlayerData(p.getName(), getTeamWithLessPlayers(), p.getInventory(), p.getLocation()));
-		for(PlayerData pd : players){
-			if(pd.getTeam() == Team.RED){
-				p.getInventory().clear();
-				p.getInventory().addItem(ironSword);
-				p.getInventory().setHelmet(lHelm);
-				p.getInventory().setChestplate(lChest);
-				p.getInventory().setLeggings(lLegs);
-				p.getInventory().setBoots(lBoots);
-			}else{
-				p.getInventory().clear();
-				p.getActivePotionEffects().add(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 0, 600));
-				p.getInventory().addItem(stoneSword);
-				p.getInventory().setChestplate(iChest);
-			}
-		}
+		p.getInventory().clear();
+		p.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 1));
+		p.teleport(getSpawn(getData(p).getTeam()));
+		
+		p.setScoreboard(sb);
+		
+		p.setGameMode(GameMode.SURVIVAL);
+		p.setFlying(false);
+		
+		MessageManager.getInstance().info(p, "You have joined arena " + getID() + " and are on the " + ChatColor.valueOf(getData(p).getTeam().toString()) + getData(p).getTeam().toString().toLowerCase() + ChatColor.YELLOW + " team!");
+		
+		if (players.size() >= 2 && !cd) start();
+	}
+	
+	public void removePlayer(Player p, boolean b){
+		players.add(new PlayerData(p.getName(), getTeamWithLessPlayers(), p.getInventory().getContents(), p.getLocation()));
+		
+		p.getInventory().clear();
+		p.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 1));
+		// Could add colored armor.
 		
 		p.teleport(getSpawn(getData(p).getTeam()));
 		
 		p.setScoreboard(sb);
 		
-		if(players.size() >= 2) start();
-	}
-	
-	public void removePlayer(Player p){
-		PlayerData pd = getData(p);
-		p.getInventory().clear();
-		p.getInventory().addItem(pd.getInventory().getContents());
-		p.getInventory().setArmorContents(pd.getInventory().getArmorContents());
+		p.setGameMode(GameMode.SURVIVAL);
+		p.setFlying(false);
 		
-		p.teleport(pd.getLocation());
+		MessageManager.getInstance().info(p, "You have joined arena " + getID() + " and are on the " + ChatColor.valueOf(getData(p).getTeam().toString()) + getData(p).getTeam().toString().toLowerCase() + ChatColor.YELLOW + " team!");
 		
-		p.setScoreboard(null);
-		
-		players.remove(pd);
-		
-		MessageManager.getInstance().info(p, "You lost the game.");
-		msg(p.getName() + " lost the game.");
+		if (players.size() >= 2 && !cd) start();
 	}
 	
 	public void start(){
+		this.state = ArenaState.COUNTING_DOWN;
+		cd = true;
 		msg("Game starting in 30 seconds!");
 		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(SettingsManager.getInstance().getPlugin(), new Runnable(){
 			public void run(){
-				Arena.this.started = true;
+				Arena.this.cd = false;
 				msg("Good luck!");
 			}
 		}, 30 * 20);
+		state = ArenaState.STARTED;
 	}
 	
 	public void stop(Team pTeam){
 		msg(pTeam != null ? pTeam.name() + " won the game!" : "The game was ended.");
-		for(PlayerData pd : players){
+		for (PlayerData pd : players) {
 			Player p = Bukkit.getServer().getPlayer(pd.getPlayerName());
-			p.getInventory().clear();
-			p.getInventory().addItem(pd.getInventory().getContents());
-			p.getInventory().setArmorContents(pd.getInventory().getArmorContents());
-			
-			p.teleport(pd.getLocation());
+			removePlayer(p, true);
 		}
 	}
 	
