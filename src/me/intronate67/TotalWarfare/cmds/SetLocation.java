f3975f0231b71014f009c161c7db8eec787e0140
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 19:01:26 -0600

    Update SetLocation.java

--- src/me/intronate67/TotalWarfare/cmds/SetLocation.java
+++ src/me/intronate67/TotalWarfare/cmds/SetLocation.java
@@ -3,59 +3,60 @@ package me.intronate67.TotalWarfare.cmds;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
-import me.intronate67.TotalWarfare.Arena;
-import me.intronate67.TotalWarfare.ArenaManager;
 import me.intronate67.TotalWarfare.ArenaManager.Team;
 import me.intronate67.TotalWarfare.MessageManager;
 import me.intronate67.TotalWarfare.SettingsManager;
 
 public class SetLocation extends SubCommand{
 	
-	public void onCommand(Player p, String[] args){
-		if(args.length < 2){
-			MessageManager.getInstance().severe(p, "You did not specify enough arguments!");
+	public void onCommand(Player p, String[] args) {
+		if (args.length < 2) {
+			MessageManager.getInstance().severe(p, "You did not specify enough arguments.");
 			return;
 		}
 		
 		int id = -1;
-		try{ id = Integer.parseInt(args[0]); }
+		
+		try { id = Integer.parseInt(args[0]); }
 		catch (Exception e) {
-			MessageManager.getInstance().severe(p, args[0] + " is not a valid arena!");
+			MessageManager.getInstance().severe(p, args[0] + " is not a valid number!");
 			return;
 		}
 		
-		Arena a = ArenaManager.getInstance().getArena(id);
-		
-		if(a == null){
+		if (SettingsManager.getInstance().<ConfigurationSection>get(id + "") == null) {
 			MessageManager.getInstance().severe(p, "There is no arena with ID " + id + "!");
 			return;
 		}
 		
 		Team team = null;
+		
 		try { team = Team.valueOf(args[1].toUpperCase()); }
-		catch(Exception e) {
-			MessageManager.getInstance().severe(p, args[1] + " is not a valid team");
+		catch (Exception e) { 
+			MessageManager.getInstance().severe(p, args[1] + " is not a valid team!");
 			return;
 		}
 		
-		ConfigurationSection s = SettingsManager.getInstance().createConfigurationSection(a.getID() + "." + team.toString().toLowerCase() + "spawn");
+		ConfigurationSection s = SettingsManager.getInstance().createConfigurationSection(id + "." + team.toString().toLowerCase() + "spawn");
 		
 		s.set("world", p.getWorld().getName());
 		s.set("x", p.getLocation().getX());
 		s.set("y", p.getLocation().getY());
 		s.set("z", p.getLocation().getZ());
 		
+		SettingsManager.getInstance().set(id + "." + team.toString().toLowerCase() + "spawn", s);
+		
 		MessageManager.getInstance().good(p, "Set " + team.toString().toLowerCase() + " spawn!");
 	}
 	
-	
-	public String name(){
+	public String name() {
 		return "setlocation";
 	}
-	public String info(){
-		return "Set a teams spawn";
+	
+	public String info() {
+		return "Set a team spawn";
 	}
-	public String[] aliases(){
+	
+	public String[] aliases() {
 		return new String[] { "s" };
 	}
 }
