36fbc5dfda46c9d7424c95f9872c5c8d6499af47
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 18:58:11 -0600

    Update SettingsManager.java

--- src/me/intronate67/TotalWarfare/SettingsManager.java
+++ src/me/intronate67/TotalWarfare/SettingsManager.java
@@ -1,6 +1,7 @@
 package me.intronate67.TotalWarfare;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -9,11 +10,11 @@ import org.bukkit.plugin.Plugin;
 
 public class SettingsManager {
 	
-	private SettingsManager(){ }
+private SettingsManager() { }
 	
 	private static SettingsManager instance = new SettingsManager();
 	
-	public static SettingsManager getInstance(){
+	public static SettingsManager getInstance() {
 		return instance;
 	}
 	
@@ -21,41 +22,47 @@ public class SettingsManager {
 	private FileConfiguration arenas;
 	private File afile;
 	
-	public void setup(Plugin p){
+	public void setup(Plugin p) {
 		this.p = p;
 		
-		if(!p.getDataFolder().exists()) p.getDataFolder().mkdir();
+		if (!p.getDataFolder().exists()) p.getDataFolder().mkdir();
 		
 		afile = new File(p.getDataFolder(), "arenas.yml");
-		if(!afile.exists()){
-			try{ afile.createNewFile(); }
-			catch (Exception e){ e.printStackTrace(); }
+		
+		boolean n = false;
+		
+		if (!afile.exists()) {
+			try { afile.createNewFile(); n = true; }
+			catch (Exception e) { e.printStackTrace(); }
 		}
 		
 		arenas = YamlConfiguration.loadConfiguration(afile);
+		
+		if (n) set("ids", new ArrayList<String>());
 	}
+	
 	@SuppressWarnings("unchecked")
-	public <T> T get(String path){
+	public <T> T get(String path) {
 		return (T) arenas.get(path);
 	}
 	
-	public void set(String path, Object value){
+	public void set(String path, Object value) {
 		arenas.set(path, value);
 		save();
 	}
 	
-	public ConfigurationSection createConfigurationSection(String path){
+	public ConfigurationSection createConfigurationSection(String path) {
 		ConfigurationSection s = arenas.createSection(path);
 		save();
 		return s;
 	}
 	
-	private void save(){
-		try{ arenas.save(afile); }
-		catch(Exception e){ e.printStackTrace(); }
+	private void save() {
+		try { arenas.save(afile); }
+		catch (Exception e) { e.printStackTrace(); }
 	}
 	
-	public Plugin getPlugin(){
+	public Plugin getPlugin() {
 		return p;
 	}
 	
