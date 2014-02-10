391fdfa2a92133ce5aa5dd60ace99fa2c6243b37
Hunter Sharpe <huntersharpe@me.com>
2014-02-09 19:01:55 -0600

    Create Reload.java

---
+++ src/me/intronate67/TotalWarfare/cmds/Reload.java
@@ -0,0 +1,25 @@
+package me.intronate67.TotalWarfare.cmds;
+
+import org.bukkit.entity.Player;
+
+import me.intronate67.TotalWarfare.ArenaManager;
+import me.intronate67.TotalWarfare.MessageManager;
+
+public class Reload extends SubCommand{
+	public void onCommand(Player p, String[] args) {
+		ArenaManager.getInstance().setup();
+		MessageManager.getInstance().good(p, "Reloaded!");
+	}
+	
+	public String name() {
+		return "reload";
+	}
+	
+	public String info() {
+		return "Reload arenas.";
+	}
+	
+	public String[] aliases() {
+		return new String[] { "r" };
+	}
+}
