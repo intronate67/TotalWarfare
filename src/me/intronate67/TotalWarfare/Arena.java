ab351bb7d5b7181b1616ff98aa8c664e715d0401
Hunter Sharpe <huntersharpe@me.com>
2014-02-11 21:16:02 -0600

    Update Arena.java

--- src/me/intronate67/TotalWarfare/Arena.java
+++ src/me/intronate67/TotalWarfare/Arena.java
@@ -1,186 +1,231 @@
 package me.intronate67.TotalWarfare;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import me.intronate67.TotalWarfare.ArenaManager.Team;
+import me.intronate67.TotalWarfare.MessageManager.MessageType;
+import me.intronate67.TotalWarfare.SettingsManager;
+import me.intronate67.TotalWarfare.listeners.SignManager;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.Scoreboard;
 
-public class Arena extends JavaPlugin{
+public class Arena {
 	
 	public enum ArenaState { DISABLED, WAITING, COUNTING_DOWN, STARTED; }
-	public boolean isHeroes = false;
-	private boolean cd = false;
-	private int id;
-	protected ArrayList<PlayerData> players = new ArrayList<PlayerData>();
-	
-	private Scoreboard sb;
-	private Objective o;
-	private Score heroes, demons;
-	private Location redspawn, bluespawn;
-	private Location capturePoint;
+
+	private int id, taskID, numPlayers, currentPlayers = 0;
 	protected ArenaState state = ArenaState.DISABLED;
+	private ArrayList<PlayerData> players = new ArrayList<PlayerData>();
+	private ArrayList<Sign> signs;
+	private Location spawnPoint, heroSpawn, demonSpawn;
+	Random rand = new Random();
 	
-	protected Arena(int id){
+	protected Arena(int id) {
+
 		this.id = id;
+		this.players = new ArrayList<PlayerData>();
+		this.numPlayers = SettingsManager.getArenas().get("arenas." + id + ".numPlayers");
 		
-		ConfigurationSection conf = SettingsManager.getInstance().get(id + "");
+		ConfigurationSection s = SettingsManager.getArenas().get("arenas." + id + ".spawn");
+		ConfigurationSection rS = SettingsManager.getArenas().get("arenas." + id + "heroSpawn");
+		ConfigurationSection bS = SettingsManager.getArenas().get("arenas." + id + "demonSpawn");
 		
-		this.redspawn = getLocation(conf.getConfigurationSection("redspawn"));
-		this.bluespawn = getLocation(conf.getConfigurationSection( "bluespawn"));
-		this.capturePoint = getCaptureLocation(conf.getConfigurationSection("capturePoint"));
+		spawnPoint = LocationUtil.locationFromConfig(s, true);
+		heroSpawn = LocationUtil.locationFromConfig(rS, true);
+		demonSpawn = LocationUtil.locationFromConfig(bS, true);
 		
 		state = ArenaState.WAITING;
-		sb = Bukkit.getServer().getScoreboardManager().getNewScoreboard();
-		o = sb.registerNewObjective("Team Scores", "dummy");
-		heroes = o.getScore(Bukkit.getServer().getOfflinePlayer(ChatColor.RED + "Heroes"));
-		demons = o.getScore(Bukkit.getServer().getOfflinePlayer(ChatColor.RED + "Demons"));
-	}
-	Arena a = ArenaManager.getInstance().getArena(id);
-	
-	private Location getLocation(ConfigurationSection path){
-		return new Location(
-				Bukkit.getServer().getWorld("world"),
-				path.getDouble("x"),
-				path.getDouble("y"),
-				path.getDouble("z"));
-	}
-	public Location getCaptureLocation(ConfigurationSection path){
-		return new Location(
-				Bukkit.getServer().getWorld("captureWorld"),
-				path.getDouble("captureX"),
-				path.getDouble("captureY"),
-				path.getDouble("captureZ"));
-	}
-	public ArenaState getState(){
-		return state;
-	}
-	
-	public int getID(){
-		return id;
-	}
-	
-	
-	public boolean isHeroes(){
-		return isHeroes;
-	}
-	public void setHeroes(boolean isHeroes){
-		this.isHeroes = isHeroes;
+		
+		this.signs = SignManager.getSigns(this);
 	}
 	
 	public Location getSpawn(Team team){
 		switch(team){
-			case RED: return redspawn;
-			case BLUE: return bluespawn;
+			case HEROES: return heroSpawn;
+			case DEMONS: return demonSpawn;
 			default: return null;
 		}
 	}
-	public Location getCapture(){
-		return capturePoint;
-	}
 	
+	public void setTeam(Team team1){
+		int n = rand.nextInt(2) + 1;
+		if(n == 1){
+			setTeam(Team.DEMONS);
+			
+		}
+			setTeam(Team.HEROES);
+	}
+
 	public Team getTeam(Player p){
 		return getData(p).getTeam();
 	}
-	
-	public void addPlayer(Player p){
-		players.add(new PlayerData(p.getName(), getTeamWithLessPlayers(), p.getInventory().getContents(), p.getLocation()));
-		
-		p.getInventory().clear();
-		p.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 1));
-		p.teleport(getSpawn(getData(p).getTeam()));
-		
-		p.setScoreboard(sb);
-		
-		p.setGameMode(GameMode.SURVIVAL);
-		p.setFlying(false);
-		
-		MessageManager.getInstance().info(p, "You have joined arena " + getID() + " and are on the " + ChatColor.valueOf(getData(p).getTeam().toString()) + getData(p).getTeam().toString().toLowerCase() + ChatColor.YELLOW + " team!");
-		
-		if (players.size() >= 2 && !cd) start();
+	public int getID() {
+		return id;
 	}
 	
-	public void removePlayer(Player p, boolean b){
-		players.add(new PlayerData(p.getName(), getTeamWithLessPlayers(), p.getInventory().getContents(), p.getLocation()));
-		
-		p.getInventory().clear();
-		p.getInventory().addItem(new ItemStack(Material.SNOW_BALL, 1));
-		// Could add colored armor.
-		
-		p.teleport(getSpawn(getData(p).getTeam()));
-		
-		p.setScoreboard(sb);
-		
-		p.setGameMode(GameMode.SURVIVAL);
-		p.setFlying(false);
-		
-		MessageManager.getInstance().info(p, "You have joined arena " + getID() + " and are on the " + ChatColor.valueOf(getData(p).getTeam().toString()) + getData(p).getTeam().toString().toLowerCase() + ChatColor.YELLOW + " team!");
-		
-		if (players.size() >= 2 && !cd) start();
+	public ArenaState getState() {
+		return state;
 	}
 	
-	public void start(){
-		this.state = ArenaState.COUNTING_DOWN;
-		cd = true;
-		msg("Game starting in 30 seconds!");
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(SettingsManager.getInstance().getPlugin(), new Runnable(){
-			public void run(){
-				Arena.this.cd = false;
-				msg("Good luck!");
-			}
-		}, 30 * 20);
-		state = ArenaState.STARTED;
-	}
-	
-	public void stop(Team pTeam){
-		msg(pTeam != null ? pTeam.name() + " won the game!" : "The game was ended.");
-		for (PlayerData pd : players) {
-			Player p = Bukkit.getServer().getPlayer(pd.getPlayerName());
-			removePlayer(p, true);
-		}
-	}
-	
-	public void addDeath(Player p){
-		Team t = getTeam(p);
-		if(t == Team.RED) demons.setScore(demons.getScore() + 1);
-		else heroes.setScore(heroes.getScore() + 1);
-	}
-	
-	private void msg(String msg){
-		for(PlayerData pd : players){
-			Player p = Bukkit.getServer().getPlayer(pd.getPlayerName());
-			MessageManager.getInstance().info(p, msg);
-		}
-	}
-	
-	private Team getTeamWithLessPlayers(){
+	private Team getTeamWithLessPlayers() {
 		int red = 0, blue = 0;
-		for(PlayerData pd : players){
-			if(pd.getTeam() == Team.RED) red ++;
+		for (PlayerData pd : players) {
+			if (pd.getTeam() == Team.HEROES) red++;
 			else blue++;
 		}
-		if(red > blue) return Team.BLUE;
-		else return Team.RED;
+		if (red > blue) return Team.DEMONS;
+		else return Team.HEROES;
 	}
 	
-	public boolean containsPlayer(Player p){
-		return getData(p) != null;
+	public int getCurrentPlayers() {
+		return currentPlayers;
 	}
-	private PlayerData getData(Player p){
-		for(PlayerData pd : players){
-			if(pd.getPlayerName().equalsIgnoreCase(p.getName())) return pd;
+	
+	public void addPlayer(Player p) {
+		
+		players.add(new PlayerData(p.getName(), getTeamWithLessPlayers(), p.getInventory().getContents(), p.getInventory().getArmorContents() , p.getLocation()));
+		if (currentPlayers >= numPlayers) {
+			MessageManager.getInstance().severe(p, "There are too many players.");
+			return;
+		}
+		
+		if (spawnPoint == null) {
+			MessageManager.getInstance().severe(p, "Spawn point not yet set.");
+			return;
+		}
+		
+		p.getInventory().clear();
+		//Default class below.
+		p.teleport(spawnPoint);
+		p.setGameMode(GameMode.SURVIVAL);
+		currentPlayers++;
+		
+		MessageManager.getInstance().info(p, "You have joined arena " + getID() + " and are on the " + ChatColor.valueOf(getData(p).getTeam().toString()) + getData(p).getTeam().toString().toLowerCase() + ChatColor.BLUE + " team!");
+		for (Sign s : signs) {
+			s.setLine(2, currentPlayers + "");
+		}
+	}
+	
+	public void spawnTele(Player p){
+		if(players.contains(p) != true){
+			return;
+		}
+		getSpawn(getData(p).getTeam());
+	}
+	
+	public void removePlayer(Player p, boolean lost) {
+		PlayerData pd = getData(p);
+		
+		p.getInventory().clear();
+		for (ItemStack i : pd.getContents()) if (i != null) p.getInventory().addItem(i);
+		p.getInventory().setArmorContents(pd.getArmorContents());
+		
+		p.teleport(pd.getLocation());
+		
+		p.setScoreboard(null);
+		
+		players.remove(pd);
+		
+		
+		if (lost) {
+			MessageManager.getInstance().info(p, "You lost the game.");
+		}
+	}
+	
+	public void start() {
+		this.state = ArenaState.COUNTING_DOWN;
+		for (Sign s : signs) {
+			s.setLine(3, getState().toString());
+			s.update();
+		}
+		
+		final Countdown c = new Countdown(30, "Game starting in %t seconds!", this, 30, 20, 10, 5, 4, 3, 2, 1);
+		this.taskID = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(TotalWarfare.getPlugin(), new Runnable() {
+			public void run() {
+				if (!c.isDone) {
+					c.run();
+					state = ArenaState.STARTED;
+					for (Sign s : signs) {
+						s.setLine(3, getState().toString());
+						s.update();
+					}
+				}
+				else Bukkit.getServer().getScheduler().cancelTask(taskID);
+				
+			}
+		}, 0, 20);
+	}
+	
+	
+	private PlayerData getData(Player p) {
+		for (PlayerData pd : players) {
+			if (pd.getPlayerName().equalsIgnoreCase(p.getPlayer().getName())) return pd;
 		}
 		return null;
 	}
+	public void stop(Player winner) {
+		MessageManager.getInstance().broadcast(MessageType.INFO, winner != null ? winner.getName() + " won the game!" : "The game was ended.");
+		for (PlayerData pd : players) {
+			Player p = Bukkit.getServer().getPlayer(pd.getPlayerName());
+			removePlayer(p, true);
+			this.state = ArenaState.WAITING;
+		}
+	}
+
+	
+	class Countdown implements Runnable {
+		
+		public boolean isDone = false;
+		public int timer;
+		public String msg;
+		public Arena a;
+		public ArrayList<Integer> countingNums;
+		public Player p;
+		
+		public Countdown(int start, String msg, Arena a, int... countingNums) {
+			this.timer = start;
+			this.msg = msg;
+			this.a = a;
+			this.countingNums = new ArrayList<Integer>();
+			for (int i : countingNums) this.countingNums.add(i);
+		}
+		
+		public void run() {
+			
+			if (timer == 0) {
+				a.sendMessage(MessageType.GOOD, "The game has begun!");
+				a.state = ArenaState.STARTED;
+				isDone = true;
+				if(players.contains(p.getName()) != true){
+					return;
+				}
+				a.spawnTele(p);
+				return;
+				
+			}
+			
+			if (countingNums.contains(timer)) {
+				a.sendMessage(MessageType.INFO, msg.replaceAll("%t", timer + ""));
+			}
+			
+			timer--;
+		}
+	}
+	
+	public boolean containsPlayer(Player p) {
+		return getData(p) != null;
+	}
+	
+	protected void sendMessage(MessageType type, String... messages) {
+		for (@SuppressWarnings("unused") PlayerData d : players) MessageManager.getInstance().broadcast(MessageType.INFO, messages);
+	}
+
 }
