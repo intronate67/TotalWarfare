904fca898440c087287008b235abeba0693cade1
Hunter Sharpe <huntersharpe@me.com>
2014-02-11 21:19:52 -0600

    Update SetLocation.java

--- src/me/intronate67/TotalWarfare/cmds/SetLocation.java
+++ src/me/intronate67/TotalWarfare/cmds/SetLocation.java
@@ -1,62 +1,55 @@
 package me.intronate67.TotalWarfare.cmds;
 
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
-
-import me.intronate67.TotalWarfare.ArenaManager.Team;
+import me.intronate67.TotalWarfare.Arena;
+import me.intronate67.TotalWarfare.ArenaManager;
 import me.intronate67.TotalWarfare.MessageManager;
 import me.intronate67.TotalWarfare.SettingsManager;
 
-public class SetLocation extends SubCommand{
-	
-	public void onCommand(Player p, String[] args) {
-		if (args.length < 2) {
-			MessageManager.getInstance().severe(p, "You did not specify enough arguments.");
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+public class SetCapture extends SubCommand{
+
+	public void onCommand(Player p, String[] args){
+		if(args.length < 1){
+			MessageManager.getInstance().severe(p, "You did not specify enough arguments!");
 			return;
 		}
 		
 		int id = -1;
-		
-		try { id = Integer.parseInt(args[0]); }
+		try{ id = Integer.parseInt(args[0]); }
 		catch (Exception e) {
-			MessageManager.getInstance().severe(p, args[0] + " is not a valid number!");
+			MessageManager.getInstance().severe(p, args[0] + " is not a valid arena!");
 			return;
 		}
 		
-		if (SettingsManager.getInstance().<ConfigurationSection>get(id + "") == null) {
+		Arena a = ArenaManager.getInstance().getArena(id);
+		
+		if(SettingsManager.getArenas().<ConfigurationSection>get(id + "") == null){
 			MessageManager.getInstance().severe(p, "There is no arena with ID " + id + "!");
 			return;
 		}
+
 		
-		Team team = null;
+		ConfigurationSection s = SettingsManager.getArenas().createConfigurationSection(a.getID() + "." + "capturePoint");
+		double x = p.getLocation().getX();
+		double y = p.getLocation().getY();
+		double z = p.getLocation().getZ();
+		s.set("captureWorld", p.getWorld().getName());
+		s.set("captureX", x);
+		s.set("captureY", y);
+		s.set("captureZ", z);
 		
-		try { team = Team.valueOf(args[1].toUpperCase()); }
-		catch (Exception e) { 
-			MessageManager.getInstance().severe(p, args[1] + " is not a valid team!");
-			return;
-		}
-		
-		ConfigurationSection s = SettingsManager.getInstance().createConfigurationSection(id + "." + team.toString().toLowerCase() + "spawn");
-		
-		s.set("world", p.getWorld().getName());
-		s.set("x", p.getLocation().getX());
-		s.set("y", p.getLocation().getY());
-		s.set("z", p.getLocation().getZ());
-		
-		SettingsManager.getInstance().set(id + "." + team.toString().toLowerCase() + "spawn", s);
-		
-		MessageManager.getInstance().good(p, "Set " + team.toString().toLowerCase() + " spawn!");
+		MessageManager.getInstance().good(p, "Set capture point for arena " + a.getID());
+	}
+	public String name(){
+		return "setcapture";
+	}
+	public String info(){
+		return "Set a capture point";
+	}
+	public String[] aliases(){
+		return new String[] { "sc" };
 	}
 	
-	public String name() {
-		return "setlocation";
-	}
-	
-	public String info() {
-		return "Set a team spawn";
-	}
-	
-	public String[] aliases() {
-		return new String[] { "s" };
-	}
 }
