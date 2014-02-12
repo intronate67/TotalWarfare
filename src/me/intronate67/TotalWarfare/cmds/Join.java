559353340b28088f24bad47dac368c1a0e6ea1e4
Hunter Sharpe <huntersharpe@me.com>
2014-02-11 21:18:55 -0600

    Update Join.java

--- src/me/intronate67/TotalWarfare/cmds/Join.java
+++ src/me/intronate67/TotalWarfare/cmds/Join.java
@@ -1,38 +1,43 @@
 package me.intronate67.TotalWarfare.cmds;
 
+import java.util.Random;
+
 import me.intronate67.TotalWarfare.Arena;
+import me.intronate67.TotalWarfare.Arena.ArenaState;
 import me.intronate67.TotalWarfare.ArenaManager;
 import me.intronate67.TotalWarfare.MessageManager;
-import me.intronate67.TotalWarfare.SettingsManager;
+import me.intronate67.TotalWarfare.MessageManager.MessageType;
 
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 public class Join extends SubCommand{
-	
+
+	Random rand = new Random();
 	public void onCommand(Player p, String[] args) {
 		if (ArenaManager.getInstance().getArena(p) != null) {
-			MessageManager.getInstance().severe(p, "You are already in a game!");
+			MessageManager.getInstance().msg(p, MessageType.BAD, "You are already in an arena!");
 			return;
 		}
 		
 		if (args.length == 0) {
-			MessageManager.getInstance().severe(p, "You must specify an arena ID.");
+			MessageManager.getInstance().msg(p, MessageType.BAD, "You must specify an arena number!");
 			return;
 		}
 		
 		int id = -1;
 		
 		try { id = Integer.parseInt(args[0]); }
-		catch (Exception e) {
-			MessageManager.getInstance().severe(p, args[0] + " is not a valid number!");
-			return;
-		}
+		catch (Exception e) { MessageManager.getInstance().msg(p, MessageType.BAD, args[0] + " is not a number!"); }
 		
 		Arena a = ArenaManager.getInstance().getArena(id);
 		
-		if (SettingsManager.getInstance().<ConfigurationSection>get(id + "") == null) {
-			MessageManager.getInstance().severe(p, "There is no arena with ID " + id + "!");
+		if (a == null) {
+			MessageManager.getInstance().msg(p, MessageType.BAD, "That arena doesn't exist!");
+			return;
+		}
+		
+		if (a.getState() == ArenaState.DISABLED || a.getState() == ArenaState.STARTED) {
+			MessageManager.getInstance().msg(p, MessageType.BAD, "That arena is " + a.getState().toString().toLowerCase() + "!");
 			return;
 		}
 		a.addPlayer(p);
