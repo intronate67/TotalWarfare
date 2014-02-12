a08c4b23a02da8d4b32deb511fcc27abaeaa574c
Hunter Sharpe <huntersharpe@me.com>
2014-02-11 21:16:36 -0600

    Update MessageManager.java

--- src/me/intronate67/TotalWarfare/MessageManager.java
+++ src/me/intronate67/TotalWarfare/MessageManager.java
@@ -1,11 +1,30 @@
 package me.intronate67.TotalWarfare;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MessageManager {
 	
 	private MessageManager(){ }
+	
+	public enum MessageType {
+
+		INFO(ChatColor.GRAY),
+		GOOD(ChatColor.GREEN),
+		BAD(ChatColor.RED);
+
+		private ChatColor color;
+
+		MessageType(ChatColor color) {
+			this.color = color;
+		}
+
+		public ChatColor getColor() {
+			return color;
+		}
+	}
 	
 	private static MessageManager instance = new MessageManager();
 	
@@ -20,6 +39,11 @@ public class MessageManager {
 		msg(s, ChatColor.BLUE, msg);
 	}
 	
+	public void msg(CommandSender sender, MessageType type, String... messages) {
+		for (String msg : messages) {
+			sender.sendMessage(prefix + type.getColor() + msg);
+		}
+	}
 	public void severe(CommandSender s, String msg){
 		msg(s, ChatColor.RED, msg);
 	}
@@ -31,5 +55,13 @@ public class MessageManager {
 	private void msg(CommandSender s, ChatColor color, String msg){
 		s.sendMessage(prefix + color + msg);
 	}
+	public void broadcast(MessageType type, String... messages) {
+		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+			for (String msg : messages) {
+				p.sendMessage(prefix + type.getColor() + msg);
+			}
+		}
+	}
+
 
 }
